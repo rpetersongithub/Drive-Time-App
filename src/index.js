59ecef3a1160f3e60e68bd09c4edb05ccb652ff8
ryanpeterson08 <ryanpeterson08@gmail.com>
2018-08-10 16:15:45 -0700

    add retina display

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
   attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
   maxZoom: 18,
   id: 'mapbox.streets',
+  detectRetina: true,
   accessToken: 'pk.eyJ1Ijoicnlhbi1wZXRlcnNvbiIsImEiOiJjajVjenA0NGkwZm84MndwODBqZmhkN2E3In0.4MPMYn7GRyEUc1ggpepcSg'
 }).addTo(map);
 
