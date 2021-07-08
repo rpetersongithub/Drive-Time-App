69743279873780c313d046e739d02d7d6a917561
ryanpeterson08 <ryanpeterson08@gmail.com>
2021-07-08 14:11:13 -0700

    updated mapbox streets api

--- src/index.js
+++ src/index.js
@@ -9,10 +9,10 @@ require('dotenv').config();
 
 
 
-var mapTiles = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+var mapTiles = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox.streets',
+    id: 'mapbox/streets-v11',
     detectRetina: true,
     accessToken: process.env.MAP_CODE
   });
